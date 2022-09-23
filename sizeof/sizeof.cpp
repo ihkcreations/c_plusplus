2f8c1793bd0246b8188ecbee5521984f98a43fd7
ihkcreations <laramin.astra@gmail.com>
2022-09-24 01:22:50 +0600

    sizeof

---
+++ sizeof/sizeof.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "size of bool: " << sizeof(bool) << std::endl;
+    std::cout << "size of char: " << sizeof(char) << std::endl;
+    std::cout << "size of int: " << sizeof(int) << std::endl;
+    std::cout << "size of float: " << sizeof(float) << std::endl;
+    std::cout << "size of double: " << sizeof(double) << std::endl;
+}
