07474c52c4da4efb6a9cc8057945607b1c9a2a20
ihkcreations <laramin.astra@gmail.com>
2022-10-07 20:22:13 +0600

    loop

---
+++ _CPP/Chapter 6 - Loops/forloop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+
+    for (int i{}; i < 10; i++)
+    {
+        std::cout << " I Love C++" << std::endl;
+    }
+
+    std::cout << std::endl;
+    for (size_t j{}; j < 15; j++)
+    {
+        std::cout << " I Love PROGRAMMING" << std::endl;
+    }
+}
---
+++ _CPP/Chapter 6 - Loops/while.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+
+    const unsigned int COUNT{10};
+    unsigned int i{};
+
+    while (i < COUNT)
+    {
+        std::cout << "I Love C++" << std::endl;
+        i++;
+    }
+}
