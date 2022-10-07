fb5e48c634c3bae8c3b077132bd4354daf2af6b0
ihkcreations <laramin.astra@gmail.com>
2022-10-08 01:07:45 +0600

    array with char

--- _CPP/Chapter 7 - Arrays/arrayChar.cpp
+++ _CPP/Chapter 7 - Arrays/arrayChar.cpp
@@ -25,4 +25,16 @@ int main()
     {
         std::cout << c;
     }
+
+    // need to mention the \0 in the array to omit
+    // garbage value
+    std::cout << std::endl;
+    char message2[]{'H', 'e', 'l', 'l', 'o', '\0'};
+    std::cout << "message2 : " << message2 << std::endl;
+
+    // need to mention the size of the array to 6 for omiting
+    // garbage value
+    std::cout << std::endl;
+    char message3[6]{'H', 'e', 'l', 'l', 'o'};
+    std::cout << "message3 : " << message3 << std::endl;
 }
