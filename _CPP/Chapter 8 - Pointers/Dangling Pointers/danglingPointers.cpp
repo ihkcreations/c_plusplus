b15f8e2f3b679ed7fa45dafac28674c7b387ac06
ihkcreations <laramin.astra@gmail.com>
2022-10-08 20:44:18 +0600

    dangling ponters

---
+++ _CPP/Chapter 8 - Pointers/Dangling Pointers/danglingPointers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    // 3 types of dangling pointers
+    // Uninitialized pointer
+    // deleted pointer
+    // multiple pointer pointing to same memory
+
+    // int *p; --> dangling uninitialized pointer
+    //-------------------------------------------------//
+}
