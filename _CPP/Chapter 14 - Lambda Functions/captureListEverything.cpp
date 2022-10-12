20d1053a26f932d5ab03ab1557af3c2fe7f130ba
ihkcreations <laramin.astra@gmail.com>
2022-10-12 22:43:12 +0600

    captureList by reference

--- _CPP/Chapter 14 - Lambda Functions/captureListEverything.cpp
+++ _CPP/Chapter 14 - Lambda Functions/captureListEverything.cpp
@@ -16,4 +16,26 @@ int main()
         std::cout << std::endl;
         c++;
     }
+
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    int d{10};
+    double e{20.5};
+    auto func3 = [&]() // here = means everything will be captured outside of the lambda function by reference
+    {
+        std::cout << "Inner value : " << d << std::endl;
+        std::cout << "Inner value : " << e << std::endl;
+    };
+
+    for (size_t i{}; i < 5; ++i)
+    {
+
+        std::cout << "Outer value : " << d << std::endl;
+        std::cout << "Outer value : " << e << std::endl;
+        func3();
+        std::cout << std::endl;
+        d++;
+        e += 0.5;
+    }
 }
