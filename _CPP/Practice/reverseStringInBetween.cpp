41e607e63aac3360a572e62649aa05a2e7b14ad1
ihkcreations <laramin.astra@gmail.com>
2022-10-17 01:54:49 +0600

    reverseString in between

--- _CPP/Practice/reverseStringInBetween.cpp
+++ _CPP/Practice/reverseStringInBetween.cpp
@@ -3,10 +3,10 @@
 int main()
 {
 
-    std::string message{"This is me mario"};
-    int messageLen = std::size(message);
+    std::string message;
 
-    char copyMessage[messageLen + 1];
+    int l{};
+    int r{};
 
     /*
     std::cout << "Copying message string to char array " << std::endl;
@@ -17,4 +17,28 @@ int main()
 
     std::cout << "char array message : " << copyMessage << std::endl;
     */
+    // iftekhar
+    std::cin >> l;
+    std::cin >> r;
+    std::cin >> message;
+
+    int newL = l - 1;
+    int newR = r - 1;
+    int messageLen = std::size(message);
+
+    std::cout << message[newL]
+              << " "
+              << message[newR] << std::endl;
+    std::cout << std::endl;
+
+    char temp;
+    for (int i = newR; i >= newL; i--)
+    {
+        temp = message[i];
+        message[i] = message[newL];
+        message[newL] = temp;
+        newL++;
+    }
+
+    std::cout << message << std::endl;
 }
