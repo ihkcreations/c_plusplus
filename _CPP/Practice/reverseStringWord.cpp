f3244d336b6047f7ac7a42a540ac88c8e5caa832
ihkcreations <laramin.astra@gmail.com>
2022-10-19 17:46:14 +0600

    reverse String word

--- _CPP/Practice/reverseStringWord.cpp
+++ _CPP/Practice/reverseStringWord.cpp
@@ -5,13 +5,18 @@ int main()
 
     std::string message;
 
-    std::cout << "Enter message : " << std::endl;
-    std::cin >> message;
+    std::cout << "Enter message : ";
+    std::getline(std::cin, message);
 
     std::cout << std::endl;
     std::cout << message << std::endl;
 
     int messageLength = std::size(message);
+    std::cout << "length : " << messageLength << std::endl;
 
-    // for ()
+    for (int i{messageLength}; i >= 0; i--)
+    {
+
+        std::cout << message[i];
+    }
 }
