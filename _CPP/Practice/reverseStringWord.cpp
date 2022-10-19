465ef3c5c9d4d7c048f55649ff3664d1248b3b8c
ihkcreations <laramin.astra@gmail.com>
2022-10-19 19:56:53 +0600

    reverse words still working

--- _CPP/Practice/reverseStringWord.cpp
+++ _CPP/Practice/reverseStringWord.cpp
@@ -19,4 +19,53 @@ int main()
 
         std::cout << message[i];
     }
+
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    std::string words;
+    std::string substring;
+
+    // std::string stringArr[]{"Ball", "Apple", "Orange"};
+    // for (int i{}; i < std::size(stringArr); i++)
+    // {
+    //     std::cout << stringArr[i] << " ";
+    // }
+
+    int spacePos = 0;
+    int wordCount = 0;
+
+    for (int i{}; i < messageLength; i++)
+    {
+
+        if (message[i] == ' ')
+        {
+            spacePos = i;
+        }
+
+        if (message[i] != ' ')
+        {
+            wordCount++;
+        }
+
+        // for (int j{}; j < messageLength; j++)
+        // {
+
+        // }
+    }
+
+    int i = 0;
+    while (i < messageLength)
+    {
+
+        if (message[i] == ' ')
+        {
+            spacePos = i;
+        }
+    }
+
+    // std::cout << spacePos << std::endl;
+    //  std::cout << wordCount << std::endl;
+
+    // std::cout << message.substr(spacePos + 1);
 }
