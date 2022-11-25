fddc4efab6cd8f85c680939421405df3c9d61a06
ihkcreations <laramin.astra@gmail.com>
2022-11-25 22:17:21 +0600

    code

--- Codeforces/318A.cpp
+++ Codeforces/318A.cpp
@@ -5,32 +5,17 @@ int main()
 
     long long int n{};
     long long int k{};
-    long int arr[5000]{};
 
     std::cin >> n;
     std::cin >> k;
-    int j{};
 
-    for (int i = 1; i <= n; i++)
+    if (k <= (n + 1) / 2)
     {
-        if (i % 2 != 0)
-        {
-            arr[j] = i;
-            j++;
-        }
+        std::cout << k * 2 - 1 << std::endl;
     }
-
-    for (int i = 1; i <= n; i++)
+    else
     {
-
-        if (i % 2 == 0)
-        {
-            arr[j] = i;
-            j++;
-        }
+        std::cout << (k - (n + 1) / 2) * 2 << std::endl;
     }
-
-    std::cout << arr[k - 1];
-
     return 0;
 }
