cb7b4ece16024218ed210f8be38c59897f02373f
ihkcreations <laramin.astra@gmail.com>
2023-09-17 01:49:56 +0600

    class part 1

--- Classes/class_part1.cpp
+++ Classes/class_part1.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
+using namespace std;
+
+// constructor variables and class variables names has to be different
+class Person
+{
+public:
+    string personName;
+    string personAge;
+    string personEmail;
+
+    Person(string name, string age, string email)
+    {
+        personName = name;
+        personAge = age;
+        personEmail = email;
+    }
+
+    void personDetail()
+    {
+        cout << "Name   : " << personName << endl;
+        cout << "Age    : " << personAge << endl;
+        cout << "Email  : " << personEmail << endl;
+        cout << endl;
+    }
+};
 
 int main()
 {
+
+    Person p1 = Person("Iftekhar", "25", "iftekhar@mail.com");
+    Person p2 = Person("Imran", "19", "imran@mail.com");
+
+    p1.personDetail();
+    p2.personDetail();
 }
